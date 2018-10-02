8a619c3ce60b2764fae48ca8adebbe84694e179f
Vlad Chernetsky <v.chernetsky@spheremall.com>
2018-10-02 18:00:42 +0300

    Change implementation to api

--- core/src/main/java/com/spheremall/core/api/ESRequest.java
+++ core/src/main/java/com/spheremall/core/api/ESRequest.java
@@ -16,8 +16,11 @@ import org.elasticsearch.client.RestClient;
 import java.io.IOException;
 import java.util.HashMap;
 
+import okhttp3.MediaType;
+
 public class ESRequest implements BaseRequest {
 
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private static final String BODY = "body";
     private SMClient client;
     private BaseResource resource;
@@ -30,11 +33,26 @@ public class ESRequest implements BaseRequest {
     @Override
     public ResponseMonada handle(Method method, String uriAppend, HashMap<String, String> queryParams) throws IOException {
 
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//
+        StringBuilder urlBuilder = getURI(uriAppend);
+
+//        Request.Builder requestBuilder = new Request.Builder()
+//                .url(client.getGatewayUrl() + urlBuilder.toString() + "?" + queryParams.get(BODY))
+//                .get();
+//
+//        for (Map.Entry<String, String> entry : client.getHeaders().entrySet()) {
+//            requestBuilder.header(entry.getKey(), entry.getValue());
+//        }
+//
+//        Request request = requestBuilder.build();
+//
+//        okhttp3.Response response = okHttpClient.newCall(request).execute();
+//        String responseString = response.body().string();
+
         HttpEntity entity = new NStringEntity(queryParams.get(BODY), ContentType.APPLICATION_JSON);
 
         RestClient restClient = getRestClient();
-
-        StringBuilder urlBuilder = getURI(uriAppend);
 
         Response response = restClient.performRequest(
                 method.name(),
