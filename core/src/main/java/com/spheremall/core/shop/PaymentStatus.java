a230bfeeac55922f4fea4bb9d3f1bde7f74d55dd
Vlad Chernetsky <v.chernetsky@spheremall.com>
2019-06-18 11:17:49 +0300

    Fix Payment Statuses

--- core/src/main/java/com/spheremall/core/shop/PaymentStatus.java
+++ core/src/main/java/com/spheremall/core/shop/PaymentStatus.java
@@ -5,7 +5,7 @@ public interface PaymentStatus {
     String SUCCESS = "2";
     String FAIL = "3";
     String CANCELED = "4";
-    String PENDING = "4";
-    String REFUSED = "5";
-    String REFUND = "6";
+    String PENDING = "5";
+    String REFUSED = "6";
+    String REFUND = "7";
 }
